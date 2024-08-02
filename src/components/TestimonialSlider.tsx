dd5611e1390cd7b4236b42960869b942ae61a717
Abrar Ahmad <kh.abrar22@gmail.com>
2024-08-02 15:51:53 +0500

    fix: Slider section overflow

--- src/components/TestimonialSlider.tsx
+++ src/components/TestimonialSlider.tsx
@@ -6,7 +6,7 @@ import Testimonials from "../data/testimonials_data.json"
 
 function TestimonialSlider() {
   return (
-    <div className="py-20 w-full dark:bg-black bg-white  dark:bg-grid-white/[0.2] bg-grid-black/[0.2] relative flex flex-col items-center justify-center">
+    <div className="py-20 w-full dark:bg-black bg-white dark:bg-grid-white/[0.2] bg-grid-black/[0.2] relative flex flex-col items-center justify-center overflow-hidden">
         <h2 className="text-3xl font-bold text-center mb-8 z-10">Hear our Harmony: Voices of success</h2>
         
         <InfiniteMovingCards
